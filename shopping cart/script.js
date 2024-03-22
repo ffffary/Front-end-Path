4b23ac09698b0bb4d408cf3769e21e1a92b7910c
Zhengfa Tang <77824078+ffffary@users.noreply.github.com>
2024-03-22 17:53:24 +0800

    shopping cart

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -1,3 +1,202 @@
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
+
+const products = [
+  {
+    id: 1,
+    name: "Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+  {
+    id: 2,
+    name: "French Macaron",
+    price: 3.99,
+    category: "Macaron",
+  },
+  {
+    id: 3,
+    name: "Pumpkin Cupcake",
+    price: 3.99,
+    category: "Cupcake",
+  },
+  {
+    id: 4,
+    name: "Chocolate Cupcake",
+    price: 5.99,
+    category: "Cupcake",
+  },
+  {
+    id: 5,
+    name: "Chocolate Pretzels (4 Pack)",
+    price: 10.99,
+    category: "Pretzel",
+  },
+  {
+    id: 6,
+    name: "Strawberry Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 7,
+    name: "Chocolate Macarons (4 Pack)",
+    price: 9.99,
+    category: "Macaron",
+  },
+  {
+    id: 8,
+    name: "Strawberry Pretzel",
+    price: 4.99,
+    category: "Pretzel",
+  },
+  {
+    id: 9,
+    name: "Butter Pecan Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 10,
+    name: "Rocky Road Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 11,
+    name: "Vanilla Macarons (5 Pack)",
+    price: 11.99,
+    category: "Macaron",
+  },
+  {
+    id: 12,
+    name: "Lemon Cupcakes (4 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+];
+
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
+      </div>
+    `;
+  }
+);
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+    })
+
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+    currentProductCount > 1 
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `
+      <div id=dessert${id} class="product">
+        <p>
+          <span class="product-count" id=product-count-for-id${id}></span>${name}
+        </p>
+        <p>${price}</p>
+      </div>
+      `;
+  }
+
+  getCounts() {
+    return this.items.length;
+  }
+
+  clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
+};
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+      cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
+    })
+  }
+);
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+  cartContainer.style.display = isCartShowing ? "block" : "none";
+});
+
+clearCartBtn.addEventListener(
+    "click", cart.clearCart.bind(cart)//bind() will create a new function
+  ) // which will be called with "this" keyword set to the provided value ?
+
+
 /* 
 steps:
 1. get elements by ID, store them in const variables
@@ -5,7 +204,75 @@ steps:
     2) buttons
     3) totals 
 
-2. 
+2. set isCartShowing to false -> for "hide/show the cart"
 
+3. fill in the products array with an array of objects - dessert info
+
+4. insert the products info into HTML by using javascript:
+destructure the properties of the object passed into the function -
+{...}
+
+5.display the products in the HTML: " .innerHTML += ` ` "
+using forEach() to iteratively fill info into those div cards
+
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
+      </div>
+    `;
+  }
+);
+
+
+6. create ShoppingCart class to initialize more instances, add 
+methods for the class
+
+add items to the shopping cart container---> addItem() {}
+
+addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const {name, price} = product; // destructure variables from product
+
+    ... more ... 
+
+  }
+
+
+7. test the class by instantiate a new object from it:
+const cart = new ShoppingCart();
+
+1) make the "add-to-cart-btn" interactive for users
+ get the elements sharing the same class name;
+ iterate through the elements array;
+ add event listener for that button;
+ call the addItem method inside of the addEventListener's arrow function;
+
+2) show the cart -- !isCartShowing
+
+8. using js to calculate: add more methods!
+1)Total number of items
+2)Subtotal
+3)Taxes
+4)total
 
 */
+
+
+
+/*
+Notes:
+1.to display things on the screen: 
+.innerHTML += ?
+
+2. to use any properties defined in the class constructor, 
+do not forget to use "this." in front of any attributes!
+
+*/ 
